bcb11b70deac744a44f5685547c9152490d189b7
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-17 18:25:58 +0700

    initial commit

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "http://localhost:4444/query"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
