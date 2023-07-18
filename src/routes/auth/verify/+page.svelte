0c9397fcf2e2db94b107ee8d863b26a7e159f1c6
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-18 19:49:33 +0700

    add verify page

---
+++ src/routes/auth/verify/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+	import ResendActivationToken from '$lib/components/auth/ResendActivationToken.svelte';
+	import VerifyActivationToken from '$lib/components/auth/VerifyActivationToken.svelte';
+
+	$: mode = $page.url.searchParams.get('mode');
+</script>
+
+{#if mode === 'resend'}
+	<ResendActivationToken />
+{:else}
+	<VerifyActivationToken />
+{/if}
