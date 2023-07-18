6657d485c22e2e8722ca96540bef733c4e128b0d
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-18 08:05:06 +0700

    add app layout to marketing

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -1,9 +1,46 @@
-<script>
+<script lang="ts">
 	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../../app.postcss';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import type { UserStore } from '$houdini';
+
+	$: user = $page.data['User'] as UserStore;
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		{#if !$user.data?.getUser}
+			<AppBar class="bg-surface-100-800-token p-4 shadow-xl">
+				<svelte:fragment slot="lead"
+					><a class="text-primary-600 text-lg font-bold" href="/">Gigachad</a
+					></svelte:fragment
+				>
+				<svelte:fragment slot="trail">
+					<a class="btn variant-ghost-primary" href="/auth?mode=signin"
+						>Sign In</a
+					>
+					<a class="btn variant-filled-primary" href="/auth?mode=signup"
+						>Sign up</a
+					>
+				</svelte:fragment>
+			</AppBar>
+		{:else}
+			<AppBar class="bg-surface-100-800-token p-4 shadow-xl">
+				<svelte:fragment slot="lead"
+					><a class="text-primary-600 text-lg font-bold" href="/">Gigachad</a
+					></svelte:fragment
+				>
+				<svelte:fragment slot="trail">
+					<a class="btn variant-ghost-primary" href="/auth?mode=signin"
+						>Sign In</a
+					>
+				</svelte:fragment>
+			</AppBar>
+		{/if}
+	</svelte:fragment>
+	<slot />
+</AppShell>
