846f007e09fad1f02feafbb4dea7849eae8b99d6
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-18 19:11:11 +0700

    update signin page

--- src/lib/components/Signin.svelte
+++ src/lib/components/Signin.svelte
@@ -2,25 +2,71 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { graphql } from '$houdini';
+	import { z } from 'zod';
+	import GraphqlMutationError from './helpers/GraphqlMutationError.svelte';
+	import InputError from './helpers/InputError.svelte';
+	import { createForm } from 'felte';
+	import type { GraphqlError } from './helpers/graphql';
+	import { focusTrap } from '@skeletonlabs/skeleton';
+	import { reporter } from '@felte/reporter-svelte';
+	import { validator } from '@felte/validator-zod';
+
+	let isFocused: boolean = true;
+
 	let currentPath = $page.url.pathname;
 
-	let email = '';
-	let password = '';
-	let error: null | string 
+	const signinSchema = z.object({
+		email: z.string().email(),
+		password: z.string().min(8).max(72)
+	});
 
-	const createActivationTokenMutation = graphql(`
-		mutation CreateActivationTokenMutation($input: ActivationTokenInput!) {
-			createActivationToken(input: $input)
+	const createAuthenticationTokenMutation = graphql(`
+		mutation CreateAuthenticationTokenMutation($input: LoginInput!) {
+			createAuthenticationToken(input: $input) {
+				tokenPlainText
+				user {
+					id
+					username
+					email
+					activated
+				}
+			}
 		}
 	`);
+
+	let graphqlMutationError: GraphqlError = null;
+	const { form, isSubmitting, isValid } = createForm<
+		z.infer<typeof signinSchema>
+	>({
+		onSubmit: async (values) => {
+			const res = await createAuthenticationTokenMutation.mutate({
+				input: {
+					email: values.email,
+					password: values.password
+				}
+			});
+			if (res.data && res.data.createAuthenticationToken) {
+				if (res.data.createAuthenticationToken.user.activated === 0) {
+					goto(`/auth/verify?email=${res.data.createAuthenticationToken.user.email}`);
+					return;
+				}
+				goto('/dashboard');
+				return;
+			}
+			graphqlMutationError = res.errors;
+		},
+		extend: [validator({ schema: signinSchema }), reporter]
+	});
 </script>
 
 <div
-	class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700"
+	class="mt-7 bg-surface-100 border border-gray-200 rounded-xl shadow-sm dark:bg-surface-800 dark:border-surface-700"
 >
 	<div class="p-4 sm:p-7">
 		<div class="text-center">
-			<h1 class="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
+			<h1 class="block text-2xl font-bold text-gray-800 dark:text-white">
+				Sign in
+			</h1>
 			<p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
 				Don't have an account yet?
 				<button
@@ -35,109 +81,50 @@
 		</div>
 
 		<div class="mt-5">
-			<div
-				class="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:mr-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ml-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600"
-			/>
-
 			<!-- Form -->
-			<form
-				on:submit|preventDefault={async () => {
-					const res = await createActivationTokenMutation.mutate({
-						input: {
-							email: email
-						}
-					});
-					error = res.errors ? res.errors[0].message : null
-				}}
-			>
+			<form use:focusTrap={isFocused} use:form>
 				<div class="grid gap-y-4">
 					<!-- Form Group -->
 					<div>
-						<label for="email" class="block text-sm mb-2 dark:text-white">Email address</label>
-						<div class="relative">
-							<input
-								type="email"
-								id="email"
-								name="email"
-								class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-								required
-								aria-describedby="email-error"
-								bind:value={email}
-							/>
-							<div
-								class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3"
-							>
-								<svg
-									class="h-5 w-5 text-red-500"
-									width="16"
-									height="16"
-									fill="currentColor"
-									viewBox="0 0 16 16"
-									aria-hidden="true"
-								>
-									<path
-										d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
-									/>
-								</svg>
-							</div>
-						</div>
-						<p class="hidden text-xs text-red-600 mt-2" id="email-error">
-							Please include a valid email address so we can get back to you
-						</p>
+						<label for="email" class="label text-sm mb-2"
+							><span>Email</span></label
+						>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							class="input"
+							title="Input (email)"
+							required
+						/>
+						<InputError forInput="email" />
 					</div>
-					<!-- End Form Group -->
-
-					<!-- Form Group -->
 					<div>
 						<div class="flex justify-between items-center">
-							<label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
+							<label for="password" class="label text-sm mb-2"
+								><span>Password</span></label
+							>
 							<a
-								class="text-sm text-blue-600 decoration-2 hover:underline font-medium"
-								href="../examples/html/recover-account.html">Forgot password?</a
+								class="text-sm text-tertiary-600 decoration-2 hover:underline font-medium"
+								href="/auth/verify?mode=resend">Forgot Password?</a
 							>
 						</div>
-						<div class="relative">
-							<input
-								type="password"
-								id="password"
-								name="password"
-								class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
-								required
-								aria-describedby="password-error"
-								bind:value={password}
-							/>
-							<div
-								class="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3"
-							>
-								<svg
-									class="h-5 w-5 text-red-500"
-									width="16"
-									height="16"
-									fill="currentColor"
-									viewBox="0 0 16 16"
-									aria-hidden="true"
-								>
-									<path
-										d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
-									/>
-								</svg>
-							</div>
-						</div>
-						<p class="hidden text-xs text-red-600 mt-2" id="password-error">
-							8+ characters required
-						</p>
+						<input
+							type="password"
+							name="password"
+							id="password"
+							class="input"
+							title="Input (password)"
+							required
+						/>
+						<InputError forInput="password" />
 					</div>
 					<!-- End Form Group -->
-
-					<!-- Checkbox -->
-					<!-- End Checkbox -->
-					{#if error }
-						<p>{error}</p>
-					{/if}
+					<GraphqlMutationError {graphqlMutationError} />
 					<button
 						type="submit"
-						class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-						>Sign in</button
+						class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-primary-500 text-white dark:text-gray-800 hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+						disabled={$isSubmitting || !$isValid}>Submit</button
 					>
 				</div>
 			</form>
