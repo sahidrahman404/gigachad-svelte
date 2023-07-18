7a71f4eea09cdee3d69b467a1c72142da622b3b2
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-18 19:49:25 +0700

    add forgot page

---
+++ src/routes/auth/forgot/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';
+	import SendForgotPasswordToken from '$lib/components/auth/SendForgotPasswordToken.svelte';
+	import ResetPassword from '$lib/components/auth/ResetPassword.svelte';
+
+	$: mode = $page.url.searchParams.get('mode');
+</script>
+
+{#if mode === 'send'}
+	<SendForgotPasswordToken />
+{:else}
+	<ResetPassword />
+{/if}
