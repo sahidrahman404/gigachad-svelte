36cf33858d5cd48e50068a724c9d380513807cf2
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-18 18:39:57 +0700

    add graphql mutation error component

---
+++ src/lib/components/helpers/GraphqlMutationError.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { GraphqlError } from './graphql';
+	export let graphqlMutationError: GraphqlError = null;
+</script>
+
+{#if graphqlMutationError}
+	<ul>
+		{#each graphqlMutationError as error}
+			<li class="text-sm space-y-1 text-error-800 dark:text-error-50">
+				{error.message}
+			</li>
+		{/each}
+	</ul>
+{/if}
