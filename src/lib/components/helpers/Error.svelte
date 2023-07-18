7c2415be2abccc256637fb13cdb13c25f9b4f24a
Sahid Rahman <sahidrahman404@gmail.com>
2023-07-18 08:48:18 +0700

    add Error component

--- src/lib/components/helpers/Error.svelte
+++ src/lib/components/helpers/Error.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	export let messages: any | string[] | [];
+	import { ValidationMessage } from '@felte/reporter-svelte';
+	export let forInput = ''
 </script>
 
 <ul
 	aria-live="polite"
 	class="text-sm space-y-1 mt-1 text-error-800 dark:text-error-50"
 >
-	{#each messages ?? [] as message}
-		<li>{message}</li>
-	{/each}
+	<ValidationMessage for={forInput} let:messages>
+		{#each messages ?? [] as message}
+			<li>{message}</li>
+		{/each}
+	</ValidationMessage>
 </ul>
